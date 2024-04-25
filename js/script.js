061cb57264f5bf7dc4f9a166bf57652429ecfb3e
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-04-25 05:31:34 -0700

    tenth commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+// ============== FADE IN ANIMATION SETTINGS ==============
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fade-up-active");
+    } else {
+      entry.target.classList.remove("fade-up-active");
+    }
+  });
+});
+
+const fadeUP = document.querySelectorAll(".fade-up");
+fadeUP.forEach((el) => observer.observe(el));
+
+// window.addEventListener("scroll", reveal);
+// function reveal() {
+//   var reveals = document.querySelectorAll(".");
+
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowheight = window.innerHeight;
+//     var revealtop = reveals[i].getBoundingClientRect().top;
+//     var revealpoint = 150;
+
+//     if (revealtop < windowheight - revealpoint) {
+//       reveals[i].classList.add("-active");
+//     } else {
+//       reveals[i].classList.remove("-active");
+//     }
+//   }
+// }
+
 // ============= SWIPER SETTINGS ================
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 24,
@@ -227,5 +259,3 @@ smallImg[3].onclick = function () {
 smallImg[4].onclick = function () {
   largeImg.src = smallImg[4].src;
 };
-
-// ============== FADE IN ANIMATION SETTINGS ==============
